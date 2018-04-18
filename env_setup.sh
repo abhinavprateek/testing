54982f526c206ff97df2ec921ad0d2de069a54ac
abhinav prateek <abhinav.avp@gmail.com>
2018-04-18 12:05:26 +0530

    Revised Env setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get -qq update && apt-get -qq --yes --force-yes install nano python python-setuptools python-dev build-essential
+apt-get update && apt-get -qq --yes --force-yes install nano python python-setuptools python-dev build-essential
 easy_install requests
 alais submit="python /home/user/testing/attempt_test_pass.py"
 alais submit.="python /home/user/testing/attempt_test_fail.py"
